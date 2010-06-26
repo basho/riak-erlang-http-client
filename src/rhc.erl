8b7d87bb8982d30beb26e6c5c978b6738a125a1f
Bryan Fink <bryan@basho.com>
2010-06-26 17:36:54 -0400

    implement mapreduce requests

--- src/rhc.erl
+++ src/rhc.erl
@@ -16,7 +16,8 @@
          mapred_stream/4, mapred_stream/5,
          mapred_bucket/3, mapred_bucket/4,
          mapred_bucket_stream/5]).
--export([list_keys_acceptor/2]).
+-export([list_keys_acceptor/2,
+         mapred_acceptor/2]).
 
 -include("raw_http.hrl").
 
@@ -159,23 +160,35 @@ set_bucket(Rhc, Bucket, Props0) ->
 mapred(Rhc, Inputs, Query) ->
     mapred(Rhc, Inputs, Query, ?DEFAULT_TIMEOUT).
 
-mapred(_Rhc, _Inputs, _Query, _Timeout) ->
-    throw(not_implemented).
+mapred(Rhc, Inputs, Query, Timeout) ->
+    {ok, ReqId} = mapred_stream(Rhc, Inputs, Query, self(), Timeout),
+    wait_for_mapred(ReqId, Timeout).
 
 mapred_stream(Rhc, Inputs, Query, ClientPid) ->
     mapred_stream(Rhc, Inputs, Query, ClientPid, ?DEFAULT_TIMEOUT).
 
-mapred_stream(_Rhc, _Inputs, _Query, _ClientPid, _Timeout) ->
-    throw(not_implemented).
+mapred_stream(Rhc, Inputs, Query, ClientPid, Timeout) ->
+    Url = mapred_url(Rhc),
+    StartRef = make_ref(),
+    Pid = spawn(rhc, mapred_acceptor, [ClientPid, StartRef]),
+    Headers = [{?HEAD_CTYPE, "application/json"}],
+    Body = encode_mapred(Inputs, Query),
+    case request_stream(Pid, post, Url, Headers, Body) of
+        {ok, ReqId} ->
+            Pid ! {ibrowse_req_id, StartRef, ReqId},
+            {ok, StartRef};
+        {error, Error} -> {error, Error}
+    end.
 
 mapred_bucket(Rhc, Bucket, Query) ->
     mapred_bucket(Rhc, Bucket, Query, ?DEFAULT_TIMEOUT).
 
-mapred_bucket(_Rhc, _Bucket, _Query, _Timeout) ->
-    throw(not_implemented).
+mapred_bucket(Rhc, Bucket, Query, Timeout) ->
+    {ok, ReqId} = mapred_bucket_stream(Rhc, Bucket, Query, self(), Timeout),
+    wait_for_mapred(ReqId, Timeout).
 
-mapred_bucket_stream(_Rhc, _Bucket, _Query, _ClientPid, _Timeout) ->
-    throw(not_implemented).
+mapred_bucket_stream(Rhc, Bucket, Query, ClientPid, Timeout) ->
+    mapred_stream(Rhc, Bucket, Query, ClientPid, Timeout).
 
 %% INTERNAL
 
@@ -197,7 +210,7 @@ root_url(#rhc{ip=Ip, port=Port}) ->
     ["http://",Ip,":",integer_to_list(Port),"/"].
 
 mapred_url(Rhc) ->
-    binary_to_list(iolist_to_binary([root_url(Rhc), "mapred/"])).
+    binary_to_list(iolist_to_binary([root_url(Rhc), "mapred/?chunked=true"])).
 
 ping_url(Rhc) ->
     binary_to_list(iolist_to_binary([root_url(Rhc), "ping/"])).
@@ -479,3 +492,158 @@ wait_for_listkeys(ReqId,Timeout,Acc) ->
     after Timeout ->
             {error, {timeout, Acc}}
     end.
+
+mapred_acceptor(Pid, PidRef) ->
+    receive
+        {ibrowse_req_id, PidRef, IbrowseRef} ->
+            mapred_acceptor(Pid,PidRef,IbrowseRef)
+    after ?DEFAULT_TIMEOUT ->
+            Pid ! {PidRef, {error, {timeout, []}}}
+    end.
+
+mapred_acceptor(Pid,PidRef,IbrowseRef) ->
+    receive
+        {ibrowse_async_headers, IbrowseRef, Status, Headers} ->
+            if Status =/= "200" ->
+                    Pid ! {PidRef, {error, {Status, Headers}}};
+               true ->
+                    {"multipart/mixed", Args} = ctype_from_headers(Headers),
+                    {"boundary", Boundary} =
+                        proplists:lookup("boundary", Args),
+                    stream_parts_acceptor(
+                      Pid, PidRef,
+                      webmachine_multipart:stream_parts(
+                        {[],stream_parts_helper(Pid,PidRef,IbrowseRef,true)},
+                        Boundary))
+            end
+    after ?DEFAULT_TIMEOUT ->
+            Pid ! {PidRef, {error, timeout}}
+    end.
+
+stream_parts_acceptor(Pid,PidRef,done_parts) ->
+    Pid ! {PidRef, done};
+stream_parts_acceptor(Pid,PidRef,{{_Name, _Param, Part},Next}) ->
+    {struct, Response} = mochijson2:decode(Part),
+    Phase = proplists:get_value(<<"phase">>, Response),
+    Res = proplists:get_value(<<"data">>, Response),
+    Pid ! {PidRef, {mapred, Phase, Res}},
+    stream_parts_acceptor(Pid,PidRef,Next()).
+
+
+stream_parts_helper(Pid, PidRef, IbrowseRef, First) ->              
+    fun() ->
+            ibrowse:stream_next(IbrowseRef),
+            receive
+                {ibrowse_async_response_end, IbrowseRef} ->
+                    {<<>>,done};
+                {ibrowse_async_response, IbrowseRef, {error, Error}} ->
+                    Pid ! {PidRef, {error, Error}},
+                    throw({error, {ibrowse, Error}});
+                {ibrowse_async_response, IbrowseRef, []} ->
+                    Fun = stream_parts_helper(Pid, PidRef, IbrowseRef, First),
+                    Fun();
+                {ibrowse_async_response, IbrowseRef, Data0} ->
+                    Data = if First ->
+                                   case Data0 of
+                                       "\n"++D -> D;
+                                       "\r\n"++D -> D;
+                                       _ -> Data0
+                                   end;
+                              true ->
+                                   Data0
+                           end,
+                    {list_to_binary(Data),
+                     stream_parts_helper(Pid, PidRef, IbrowseRef,false)}
+            after ?DEFAULT_TIMEOUT ->
+                    Pid ! {PidRef, {error, timeout}},
+                    throw({error, {ibrowse, timeout}})
+            end
+    end.
+
+%% @private
+wait_for_mapred(ReqId, Timeout) ->
+    wait_for_mapred(ReqId,Timeout,orddict:new()).
+%% @private
+wait_for_mapred(ReqId, Timeout, Acc) ->
+    receive
+        {ReqId, done} -> {ok, orddict:to_list(Acc)};
+        {ReqId, {mapred,Phase,Res}} ->
+            wait_for_mapred(ReqId,Timeout,orddict:append_list(Phase,Res,Acc));
+        {ReqId, {error, Reason}} -> {error, Reason}
+    after Timeout ->
+            {error, {timeout, orddict:to_list(Acc)}}
+    end.
+
+
+encode_mapred(Inputs, Query) ->
+    mochijson2:encode(
+      {struct, [{<<"inputs">>, encode_mapred_inputs(Inputs)},
+                {<<"query">>, encode_mapred_query(Query)}]}).
+
+encode_mapred_inputs(Bucket) when is_binary(Bucket) ->
+    Bucket;
+encode_mapred_inputs(Keylist) when is_list(Keylist) ->
+    [ normalize_mapred_input(I) || I <- Keylist ].
+
+normalize_mapred_input({Bucket, Key})
+  when is_binary(Bucket), is_binary(Key) ->
+    [Bucket, Key];
+normalize_mapred_input({{Bucket, Key}, KeyData})
+  when is_binary(Bucket), is_binary(Key) ->
+    [Bucket, Key, KeyData];
+normalize_mapred_input([Bucket, Key])
+  when is_binary(Bucket), is_binary(Key) ->
+    [Bucket, Key];
+normalize_mapred_input([Bucket, Key, KeyData])
+  when is_binary(Bucket), is_binary(Key) ->
+    [Bucket, Key, KeyData].
+
+encode_mapred_query(Query) when is_list(Query) ->
+    [ encode_mapred_phase(P) || P <- Query ].
+
+encode_mapred_phase({MR, Fundef, Arg, Keep}) when MR =:= map;
+                                                  MR =:= reduce ->
+    Type = if MR =:= map -> <<"map">>;
+              MR =:= reduce -> <<"reduce">>
+           end,
+    {Lang, Json} = case Fundef of
+                       {modfun, Mod, Fun} ->
+                           {<<"erlang">>,
+                            [{<<"module">>,
+                              list_to_binary(atom_to_list(Mod))},
+                             {<<"function">>,
+                              list_to_binary(atom_to_list(Fun))}]};
+                       {jsfun, Name} ->
+                           {<<"javascript">>,
+                            [{<<"name">>, Name}]};
+                       {jsanon, {Bucket, Key}} ->
+                           {<<"javascript">>,
+                            [{<<"bucket">>, Bucket},
+                             {<<"key">>, Key}]};
+                       {jsanon, Source} ->
+                           {<<"javascript">>,
+                            [{<<"source">>, Source}]}
+                   end,
+    {struct,
+     [{Type,
+       {struct, [{<<"language">>, Lang},
+                 {<<"arg">>, Arg},
+                 {<<"keep">>, Keep}
+                 |Json
+                ]}
+      }]};
+encode_mapred_phase({link, Bucket, Tag, Keep}) ->
+    {struct,
+     [{<<"link">>,
+       {struct, [{<<"bucket">>, if Bucket =:= '_' -> <<"_">>;
+                                   true           -> Bucket
+                                end},
+                 {<<"tag">>, if Tag =:= '_' -> <<"_">>;
+                                true        -> Tag
+                             end},
+                 {<<"keep">>, Keep}
+                 ]}
+       }]}.
+
+
+                           
