b69e4f40bc4e714706e3ebdb63cc01db726b21fc
Daniel Néri <dne@mayonnaise.net>
2010-11-13 11:55:50 +0100

    Add missing parenthesis to EDoc @spec for rhc:search/5

--- src/rhc.erl
+++ src/rhc.erl
@@ -345,7 +345,7 @@ search(Rhc, Bucket, SearchQuery) ->
 %%      the allowed formats for `MRQuery'. This command will return an error 
 %%      unless executed against a Riak Search cluster.
 %% @spec search(rhc(), bucket(), string(), 
-%%       [rhc_mapred:query_part()], integer() ->
+%%       [rhc_mapred:query_part()], integer()) ->
 %%       {ok, [rhc_mapred:phase_result()]}|{error, term()}
 search(Rhc, Bucket, SearchQuery, MRQuery, Timeout) ->
     Inputs = {modfun, riak_search, mapred_search, [Bucket, SearchQuery]},
