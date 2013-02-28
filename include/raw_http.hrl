79fc2774f993cb513b7a29d4b11deade82994c08
Sean Cribbs <sean@basho.com>
2013-02-28 15:37:43 -0600

    Support all the bucket properties. Needed for basho/riak_test#226.

--- include/raw_http.hrl
+++ include/raw_http.hrl
@@ -10,13 +10,15 @@
 %% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 %% KIND, either express or implied.  See the License for the
 %% specific language governing permissions and limitations
-%% under the License.    
+%% under the License.
 
 %% Constants used by the raw_http resources
 %% original source at
 %% http://github.com/basho/riak_kv/blob/master/src/riak_kv_wm_raw.hrl
 
+%%======================================================================
 %% Names of riak_object metadata fields
+%%======================================================================
 -define(MD_CTYPE,    <<"content-type">>).
 -define(MD_CHARSET,  <<"charset">>).
 -define(MD_ENCODING, <<"content-encoding">>).
@@ -25,7 +27,9 @@
 -define(MD_LASTMOD,  <<"X-Riak-Last-Modified">>).
 -define(MD_USERMETA, <<"X-Riak-Meta">>).
 
+%%======================================================================
 %% Names of HTTP header fields
+%%======================================================================
 -define(HEAD_CTYPE,           "Content-Type").
 -define(HEAD_VCLOCK,          "X-Riak-Vclock").
 -define(HEAD_LINK,            "Link").
@@ -33,25 +37,60 @@
 -define(HEAD_CLIENT,          "X-Riak-ClientId").
 -define(HEAD_USERMETA_PREFIX, "x-riak-meta-").
 
+%%======================================================================
+%% JSON keys/values
+%%======================================================================
+
+%% Top-level keys in JSON bucket responses
+-define(JSON_PROPS,      <<"props">>).
+-define(JSON_KEYS,       <<"keys">>).
+
 %% Names of JSON fields in bucket properties
--define(JSON_PROPS,   <<"props">>).
--define(JSON_KEYS,    <<"keys">>).
--define(JSON_LINKFUN, <<"linkfun">>).
--define(JSON_MOD,     <<"mod">>).
--define(JSON_FUN,     <<"fun">>).
--define(JSON_CHASH,   <<"chash_keyfun">>).
--define(JSON_JSFUN,    <<"jsfun">>).
--define(JSON_JSANON,   <<"jsanon">>).
--define(JSON_JSBUCKET, <<"bucket">>).
--define(JSON_JSKEY,    <<"key">>).
 -define(JSON_ALLOW_MULT, <<"allow_mult">>).
--define(JSON_N_VAL,    <<"n_val">>).
--define(JSON_PRECOMMIT, <<"precommit">>).
--define(JSON_SEARCH, <<"search">>).
+-define(JSON_BACKEND,    <<"backend">>).
+-define(JSON_BASIC_Q,    <<"basic_quorum">>).
+-define(JSON_BIG_VC,     <<"big_vclock">>).
+-define(JSON_CHASH,      <<"chash_keyfun">>).
+-define(JSON_DW,         <<"dw">>).
+-define(JSON_LINKFUN,    <<"linkfun">>).
+-define(JSON_LWW,        <<"last_write_wins">>).
+-define(JSON_NF_OK,      <<"notfound_ok">>).
+-define(JSON_N_VAL,      <<"n_val">>).
+-define(JSON_OLD_VC,     <<"old_vclock">>).
 -define(JSON_POSTCOMMIT, <<"postcommit">>).
+-define(JSON_PR,         <<"pr">>).
+-define(JSON_PRECOMMIT,  <<"precommit">>).
+-define(JSON_PW,         <<"pw">>).
+-define(JSON_R,          <<"r">>).
+-define(JSON_REPL,       <<"repl">>).
+-define(JSON_RW,         <<"rw">>).
+-define(JSON_SEARCH,     <<"search">>).
+-define(JSON_SMALL_VC,   <<"small_vclock">>).
+-define(JSON_W,          <<"w">>).
+-define(JSON_YOUNG_VC,   <<"young_vclock">>).
 
+%% Valid quorum property values in JSON
+-define(JSON_ALL,        <<"all">>).
+-define(JSON_ONE,        <<"one">>).
+-define(JSON_QUORUM,     <<"quorum">>).
 
+%% Valid 'repl' property values in JSON
+-define(JSON_BOTH,       <<"both">>).
+-define(JSON_FULLSYNC,   <<"fullsync">>).
+-define(JSON_OFF,        <<"off">>).
+-define(JSON_REALTIME,   <<"realtime">>).
+
+%% MapReduce JSON fields
+-define(JSON_MOD,        <<"mod">>). %% also used by bucket props
+-define(JSON_FUN,        <<"fun">>). %% also used by bucket props
+-define(JSON_JSANON,     <<"jsanon">>).
+-define(JSON_JSBUCKET,   <<"bucket">>).
+-define(JSON_JSFUN,      <<"jsfun">>).
+-define(JSON_JSKEY,      <<"key">>).
+
+%%======================================================================
 %% Names of HTTP query parameters
+%%======================================================================
 -define(Q_PROPS, "props").
 -define(Q_KEYS,  "keys").
 -define(Q_FALSE, "false").
--- src/rhc_bucket.erl
+++ src/rhc_bucket.erl
@@ -31,18 +31,85 @@
 
 erlify_props(Props) ->
     lists:flatten([ erlify_prop(K, V) || {K, V} <- Props ]).
-erlify_prop(?JSON_N_VAL, N) -> {n_val, N};
 erlify_prop(?JSON_ALLOW_MULT, AM) -> {allow_mult, AM};
-erlify_prop(?JSON_PRECOMMIT, P) -> {precommit, P};
-erlify_prop(?JSON_SEARCH, B) -> {search, B};
+erlify_prop(?JSON_BACKEND, B) -> {backend, B};
+erlify_prop(?JSON_BASIC_Q, B) -> {basic_quorum, B};
+erlify_prop(?JSON_BIG_VC, I) -> {big_vclock, I};
+erlify_prop(?JSON_CHASH, CH) -> {chash_keyfun, erlify_chash(CH)};
+erlify_prop(?JSON_DW, DW) -> {dw, erlify_quorum(DW)};
+erlify_prop(?JSON_LINKFUN, LF) -> {linkfun, erlify_linkfun(LF)};
+erlify_prop(?JSON_LWW, LWW) -> {last_write_wins, LWW};
+erlify_prop(?JSON_NF_OK, NF) -> {notfound_ok, NF};
+erlify_prop(?JSON_N_VAL, N) -> {n_val, N};
+erlify_prop(?JSON_OLD_VC, I) -> {old_vclock, I};
 erlify_prop(?JSON_POSTCOMMIT, P) -> {postcommit, P};
+erlify_prop(?JSON_PR, PR) -> {dw, erlify_quorum(PR)};
+erlify_prop(?JSON_PRECOMMIT, P) -> {precommit, P};
+erlify_prop(?JSON_PW, PW) -> {pw, erlify_quorum(PW)};
+erlify_prop(?JSON_R, R) -> {r, erlify_quorum(R)};
+erlify_prop(?JSON_REPL, R) -> {repl, erlify_repl(R)};
+erlify_prop(?JSON_RW, RW) -> {rw, erlify_quorum(RW)};
+erlify_prop(?JSON_SEARCH, B) -> {search, B};
+erlify_prop(?JSON_SMALL_VC, I) -> {small_vclock, I};
+erlify_prop(?JSON_W, W) -> {w, erlify_quorum(W)};
+erlify_prop(?JSON_YOUNG_VC, I) -> {young_vclock, I};
 erlify_prop(_Ignore, _) -> [].
+
+erlify_quorum(?JSON_ALL) -> all;
+erlify_quorum(?JSON_QUORUM) -> quorum;
+erlify_quorum(?JSON_ONE) -> one;
+erlify_quorum(I) when is_integer(I) -> I;
+erlify_quorum(_) -> undefined.
+
+erlify_repl(?JSON_OFF) -> off;
+erlify_repl(?JSON_REALTIME) -> realtime;
+erlify_repl(?JSON_FULLSYNC) -> fullsync;
+erlify_repl(?JSON_BOTH) -> both;
+%% erlify_repl(true) -> both;
+%% erlify_repl(false) -> off;
+erlify_repl(_) -> undefined.
+
+erlify_chash({struct, [{?JSON_MOD, Mod}, {?JSON_FUN, Fun}]}=Struct) ->
+    try
+        {binary_to_existing_atom(Mod, utf8), binary_to_existing_atom(Fun, utf8)}
+    catch
+        error:badarg ->
+            error_logger:warning_msg("Creating modfun atoms from JSON bucket property! ~p", [Struct]),
+            {binary_to_atom(Mod, utf8), binary_to_atom(Fun, utf8)}
+    end.
+
+erlify_linkfun(Struct) ->
+    {Mod, Fun} = erlify_chash(Struct),
+    {modfun, Mod, Fun}.
 
 httpify_props(Props) ->
     lists:flatten([ httpify_prop(K, V) || {K, V} <- Props ]).
-httpify_prop(n_val, N) -> {?JSON_N_VAL, N};
 httpify_prop(allow_mult, AM) -> {?JSON_ALLOW_MULT, AM};
-httpify_prop(precommit, P) -> {?JSON_PRECOMMIT, P};
-httpify_prop(search, B) -> {?JSON_SEARCH, B};
+httpify_prop(backend, B) -> {?JSON_BACKEND, B};
+httpify_prop(basic_quorum, B) -> {?JSON_BASIC_Q, B};
+httpify_prop(big_vclock, VC) -> {?JSON_BIG_VC, VC};
+httpify_prop(chash_keyfun, MF) -> {?JSON_CHASH, httpify_modfun(MF)};
+httpify_prop(dw, Q) -> {?JSON_DW, Q};
+httpify_prop(last_write_wins, LWW) -> {?JSON_LWW, LWW};
+httpify_prop(linkfun, LF) -> {?JSON_LINKFUN, httpify_modfun(LF)};
+httpify_prop(n_val, N) -> {?JSON_N_VAL, N};
+httpify_prop(notfound_ok, NF) -> {?JSON_NF_OK, NF};
+httpify_prop(old_vclock, VC) -> {?JSON_OLD_VC, VC};
 httpify_prop(postcommit, P) -> {?JSON_POSTCOMMIT, P};
+httpify_prop(pr, Q) -> {?JSON_PR, Q};
+httpify_prop(precommit, P) -> {?JSON_PRECOMMIT, P};
+httpify_prop(pw, Q) -> {?JSON_PW, Q};
+httpify_prop(r, Q) -> {?JSON_R, Q};
+httpify_prop(repl, R) -> {?JSON_REPL, R};
+httpify_prop(rw, Q) -> {?JSON_RW, Q};
+httpify_prop(search, B) -> {?JSON_SEARCH, B};
+httpify_prop(small_vclock, VC) -> {?JSON_SMALL_VC, VC};
+httpify_prop(w, Q) -> {?JSON_W, Q};
+httpify_prop(young_vclock, VC) -> {?JSON_YOUNG_VC, VC};
 httpify_prop(_Ignore, _) -> [].
+
+httpify_modfun({modfun, M, F}) ->
+    httpify_modfun({M, F});
+httpify_modfun({M, F}) ->
+    {struct, [{?JSON_MOD, atom_to_binary(M, utf8)},
+              {?JSON_FUN, atom_to_binary(F, utf8)}]}.
