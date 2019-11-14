e4b4206fcad8e391f08e6ae85a555c262b8d8794
Martin Sumner <martin.sumner@adaptip.co.uk>
2019-11-14 15:36:19 +0000

    HTTP client doesn't work with typed buckets
    {Type, Bucket} is not Bucket when Bucket is typed

--- src/rhc.erl
+++ src/rhc.erl
@@ -325,8 +325,9 @@ aae_fetch_clocks(Rhc, NVal, Segments) ->
                      key_range(), tree_size(),
                      segment_filter(), modified_range(), hash_method()) ->
                             {ok, {tree, Tree::any()}} | {error, any()}.
-aae_range_tree(Rhc, Bucket, KeyRange, TreeSize, SegmentFilter, ModifiedRange, HashMethod) ->
-    {Type, Bucket} = extract_bucket_type(Bucket),
+aae_range_tree(Rhc, BucketAndType, KeyRange,
+                TreeSize, SegmentFilter, ModifiedRange, HashMethod) ->
+    {Type, Bucket} = extract_bucket_type(BucketAndType),
     Url =
         lists:flatten(
           [root_url(Rhc),
@@ -348,8 +349,8 @@ aae_range_tree(Rhc, Bucket, KeyRange, TreeSize, SegmentFilter, ModifiedRange, Ha
 -spec aae_range_clocks(rhc(), riakc_obj:bucket(), key_range(), segment_filter(), modified_range()) ->
                               {ok, {keysclocks, [{{riakc_obj:bucket(), riakc_obj:key()}, binary()}]}} |
                               {error, any()}.
-aae_range_clocks(Rhc, Bucket, KeyRange, SegmentFilter, ModifiedRange) ->
-    {Type, Bucket} = extract_bucket_type(Bucket),
+aae_range_clocks(Rhc, BucketAndType, KeyRange, SegmentFilter, ModifiedRange) ->
+    {Type, Bucket} = extract_bucket_type(BucketAndType),
     Url =
         lists:flatten(
           [root_url(Rhc),
@@ -391,8 +392,8 @@ aae_range_clocks(Rhc, Bucket, KeyRange, SegmentFilter, ModifiedRange) ->
                            {ok, {keys, list({riakc_obj:key(), pos_integer()})}} |
                            {error, any()} when
       Query :: {sibling_count, pos_integer()} | {object_size, pos_integer()}.
-aae_find_keys(Rhc, Bucket, KeyRange, ModifiedRange, Query) ->
-    {Type, Bucket} = extract_bucket_type(Bucket),
+aae_find_keys(Rhc, BucketAndType, KeyRange, ModifiedRange, Query) ->
+    {Type, Bucket} = extract_bucket_type(BucketAndType),
     {Prefix, Suffix} =
         case element(1, Query) of
             sibling_count -> {"siblings", "counts"};
@@ -439,8 +440,8 @@ aae_find_keys(Rhc, Bucket, KeyRange, ModifiedRange, Query) ->
 -spec aae_object_stats(rhc(), riakc_obj:bucket(), key_range(), modified_range()) ->
                            {ok, {stats, list({Key::atom(), Val::atom() | list()})}} |
                            {error, any()}.
-aae_object_stats(Rhc, Bucket, KeyRange, ModifiedRange) ->
-    {Type, Bucket} = extract_bucket_type(Bucket),
+aae_object_stats(Rhc, BucketAndType, KeyRange, ModifiedRange) ->
+    {Type, Bucket} = extract_bucket_type(BucketAndType),
     Url =
         lists:flatten(
           [root_url(Rhc),
